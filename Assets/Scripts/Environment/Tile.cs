63191dcd221f44737c157869a1eb7f35605025a1
JR-Morgan <45512892+JR-Morgan@users.noreply.github.com>
2021-05-28 23:44:59 +0100

    Added Navmesh Components

---
+++ Assets/Scripts/Environment/Tile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class Tile : MonoBehaviour
+{
+    public TileGrid Grid { get; internal set; }
+}
