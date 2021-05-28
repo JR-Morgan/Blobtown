3dc2eb085a6bd4ee949d318b8b6f3d5fdd3e3e58
JR-Morgan <45512892+JR-Morgan@users.noreply.github.com>
2021-05-29 00:16:53 +0100

    Updated Agent AI

--- Assets/Scripts/Agent AI/AgentAI.cs
+++ Assets/Scripts/Agent AI/AgentAI.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[SelectionBase, DisallowMultipleComponent]
+[AddComponentMenu("Simulation/Agent")]
+[RequireComponent(typeof(NavMeshAgent))]
 public class AgentAI : MonoBehaviour
 {
 
     private AgentActor agentActor;
+    private NavMeshAgent navAgent;
+
     public GameObject home;
+    
+    private void Awake()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
 
     private void Start()
     {
@@ -27,7 +38,9 @@ public class AgentAI : MonoBehaviour
 
     public void SetDestination(Vector3 destinationObject)
     {
-        TileGrid.Instance.TileAtWorldPosition(destinationObject);
+        Tile tile = TileGrid.Instance.TileAtWorldPosition(destinationObject);
+
+        navAgent.SetDestination(tile.transform.position);
     }
 
 }
