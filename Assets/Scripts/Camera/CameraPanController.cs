be6e830f9b121d8d8639f53c9b59ae83e7aba837
Owen Craig <owencraig1999@gmail.com>
2021-05-30 02:10:49 +0100

    Camera pan stuff

--- Assets/Scripts/Camera/CameraPanController.cs
+++ Assets/Scripts/Camera/CameraPanController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraPanController : MonoBehaviour
 {
     private Camera _camera;
+    private Vector3 initialVector;
 
     [SerializeField]
     private Transform targetTransform;
@@ -15,14 +16,25 @@ public class CameraPanController : MonoBehaviour
     [SerializeField]
     float smoothFactor = 0.5f;
 
-    private Vector3 cameraOffset;
+    //[SerializeField]
+    private float rotateSpeed = 20;
+
+    //[SerializeField]
+    private float angleMax = 30f;
+
+    private float angleMin = 10f;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 200;
         _camera = Camera.main;
-        cameraOffset = _camera.transform.position - targetTransform.position;
+        //cameraOffset = _camera.transform.position - targetTransform.position;
+        initialVector = transform.position - targetTransform.position;
+        initialVector.y = 0;
     }
 
     // Update is called once per frame
@@ -44,14 +56,39 @@ public class CameraPanController : MonoBehaviour
         targetTransform.Translate(cameraMoveSpeedModifier * Time.deltaTime * move);
 
 
-        float rotateInputX;
-        rotateInputX = Input.GetAxis("RotateHorizontal") * cameraMoveSpeedModifier * Time.deltaTime * _camera.orthographicSize;
-        //moveInputX = Input.GetAxis("Horizontal") * cameraMoveSpeedModifier * Time.deltaTime * targetTransform.position.x;
-        float rotateInputY;
-        rotateInputY = Input.GetAxis("RotateVertical") * cameraMoveSpeedModifier * Time.deltaTime * _camera.orthographicSize;
-        //moveInputY = Input.GetAxis("Vertical") * cameraMoveSpeedModifier * Time.deltaTime * targetTransform.position.y;
+        transform.RotateAround(targetTransform.position, Vector3.up, rotateSpeed * Time.deltaTime * Input.GetAxis("RotateHorizontal"));
 
-        _camera.transform.Translate(new Vector3(rotateInputX, rotateInputY));
+
+
+        if(Input.GetAxis("RotateVertical") != 0)
+        {
+            float rotateDegrees = rotateSpeed * -Input.GetAxis("RotateVertical");
+            Vector3 direction = targetTransform.position - transform.position; // Vector from camera to player
+            Vector3 directionRight = Vector3.Cross(direction, Vector3.up); // right vector of the vector above
+
+
+            float angle = Vector3.Angle(Vector3.up, direction);
+
+            if (Vector3.Cross(Vector3.up, direction).z < 0) angle = -angle;
+            float newAngle = Mathf.Clamp(angle + rotateDegrees, angleMin, angleMax);
+
+            rotateDegrees = newAngle - rotateDegrees;
+
+            transform.RotateAround(targetTransform.position, directionRight, rotateDegrees);
+        }
+        
+
+
+
+
+
+        //float rotateInputX;;
+        //rotateInputX = Input.GetAxis("RotateHorizontal") * cameraMoveSpeedModifier * Time.deltaTime * _camera.transform.right;
+        //moveInputX = Input.GetAxis("Horizontal") * cameraMoveSpeedModifier * Time.deltaTime * targetTransform.position.x;
+        //float rotateInputY;
+        //rotateInputY = Input.GetAxis("RotateVertical") * cameraMoveSpeedModifier * Time.deltaTime * _camera.orthographicSize;
+        //moveInputY = Input.GetAxis("Vertical") * cameraMoveSpeedModifier * Time.deltaTime * targetTransform.position.y;
+        //_camera.transform.Translate(new Vector3(rotateInputX, rotateInputY));
 
 
 
