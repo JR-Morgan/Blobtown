e5f7d76d466345e3630b19f2b1934f7a79f51b1f
Tom Claxton <tpc135@hotmail.co.uk>
2021-05-30 15:14:13 +0100

    Revert "Camera pan stuff"
    This reverts commit 4634813d3a3e4b1a03db19a3e8c83bfb8968840b.

--- Assets/Scripts/Camera/CameraPanController.cs
+++ Assets/Scripts/Camera/CameraPanController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CameraPanController : MonoBehaviour
 {
     private Camera _camera;
-    private Vector3 initialVector;
 
     [SerializeField]
     private Transform targetTransform;
@@ -16,25 +15,14 @@ public class CameraPanController : MonoBehaviour
     [SerializeField]
     float smoothFactor = 0.5f;
 
-    //[SerializeField]
-    private float rotateSpeed = 20;
-
-    //[SerializeField]
-    private float angleMax = 30f;
-
-    private float angleMin = 10f;
-
-
-
+    private Vector3 cameraOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 200;
         _camera = Camera.main;
-        //cameraOffset = _camera.transform.position - targetTransform.position;
-        initialVector = transform.position - targetTransform.position;
-        initialVector.y = 0;
+        cameraOffset = _camera.transform.position - targetTransform.position;
     }
 
     // Update is called once per frame
@@ -56,39 +44,14 @@ public class CameraPanController : MonoBehaviour
         targetTransform.Translate(cameraMoveSpeedModifier * Time.deltaTime * move);
 
 
-        transform.RotateAround(targetTransform.position, Vector3.up, rotateSpeed * Time.deltaTime * Input.GetAxis("RotateHorizontal"));
-
-
-
-        if(Input.GetAxis("RotateVertical") != 0)
-        {
-            float rotateDegrees = rotateSpeed * -Input.GetAxis("RotateVertical");
-            Vector3 direction = targetTransform.position - transform.position; // Vector from camera to player
-            Vector3 directionRight = Vector3.Cross(direction, Vector3.up); // right vector of the vector above
-
-
-            float angle = Vector3.Angle(Vector3.up, direction);
-
-            if (Vector3.Cross(Vector3.up, direction).z < 0) angle = -angle;
-            float newAngle = Mathf.Clamp(angle + rotateDegrees, angleMin, angleMax);
-
-            rotateDegrees = newAngle - rotateDegrees;
-
-            transform.RotateAround(targetTransform.position, directionRight, rotateDegrees);
-        }
-        
-
-
-
-
-
-        //float rotateInputX;;
-        //rotateInputX = Input.GetAxis("RotateHorizontal") * cameraMoveSpeedModifier * Time.deltaTime * _camera.transform.right;
+        float rotateInputX;
+        rotateInputX = Input.GetAxis("RotateHorizontal") * cameraMoveSpeedModifier * Time.deltaTime * _camera.orthographicSize;
         //moveInputX = Input.GetAxis("Horizontal") * cameraMoveSpeedModifier * Time.deltaTime * targetTransform.position.x;
-        //float rotateInputY;
-        //rotateInputY = Input.GetAxis("RotateVertical") * cameraMoveSpeedModifier * Time.deltaTime * _camera.orthographicSize;
+        float rotateInputY;
+        rotateInputY = Input.GetAxis("RotateVertical") * cameraMoveSpeedModifier * Time.deltaTime * _camera.orthographicSize;
         //moveInputY = Input.GetAxis("Vertical") * cameraMoveSpeedModifier * Time.deltaTime * targetTransform.position.y;
-        //_camera.transform.Translate(new Vector3(rotateInputX, rotateInputY));
+
+        _camera.transform.Translate(new Vector3(rotateInputX, rotateInputY));
 
 
 
