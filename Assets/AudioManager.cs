0d2ee6216bfc9407b9875f2e63fa42e174214b74
Tom Claxton <tpc135@hotmail.co.uk>
2021-06-01 19:08:34 +0100

    Audio now fades

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -18,15 +18,48 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    public void Play (string name, bool loop)
+    public void Play (string name, bool loop, float volume)
     {
         Sound s = Array.Find(audioClips, sound => sound.name == name);
-        s.source.Play();
+        if (!s.source.isPlaying)
+        {
+            s.source.Play();
+        }
+
         s.source.loop = loop;
+
+        s.source.volume = 0f;
+
+        StartCoroutine(FadeAudioSource.StartFade(s.source, 10f, volume));
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Play("Layer1", true, 1);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Play("Layer2", true, 1);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Play("Layer3", true, 1);
+
+        }
+
     }
 
     void Start()
     {
-        Play("Layer1", true);
+        Play("Layer1", true, 0);
+        Play("Layer2", true, 0);
+        Play("Layer3", true, 0);
     }
 }
